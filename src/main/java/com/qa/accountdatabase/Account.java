f35968b0b0522e1177af2597469506afe1620156
BradHudsonGrant <braddershg@googlemail.com>
2018-11-15 10:20:05 +0000

    first commit

---
+++ src/main/java/com/qa/accountdatabase/Account.java
@@ -0,0 +1,46 @@
+package com.qa.accountdatabase;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+	
+	@Id
+	private long accountID;
+	
+	@Column(length = 50)
+	private String firstName;
+	@Column(length = 50)
+	private String lastName;
+	@Column(length = 50)
+	private String accountNumber;
+	
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	
+}
